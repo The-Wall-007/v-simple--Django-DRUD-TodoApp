7ef7d435c527e9b75449b2916e75accd3a603fcb
Dhaval <dvlpatel2410@gmail.com>
2020-08-26 20:18:14 +0530

    A simple doto app performing CRUD in Django

---
+++ Todo_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . views import (list_todos,
+                    detail_view,
+                    create_todo,
+                    update_todo,
+                    delete_todo
+)
+
+urlpatterns = [
+    path('', list_todos),
+    path('create/', create_todo),
+    path('<id>/', detail_view),
+    path('<id>/update/', update_todo),
+    path('<id>/delete/', delete_todo),
+
+
+]
